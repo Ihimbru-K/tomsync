ef149070f2907f804eb6355c46ef9de30138cf15
Ihimbru-k <kanihims25@gmail.com>
2024-04-28 15:50:03 +0100

    setup basic spring config and yml file

---
+++ src/main/java/com/ihims/Main.java
@@ -0,0 +1,35 @@
+package com.ihims;
+
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+
+
+
+
+///**
+// SpringBootApplication controller basically autoconfigures a new springboot project
+//
+// *///
+@SpringBootApplication //this annotation declares that this is a spring application
+@RestController
+public class Main {
+    public static void main(String[] args) {
+        //System.out.println("Hello world");
+        SpringApplication.run(Main.class, args);
+
+    //tomcat is an embedded web server which provides a pure Java http web server
+        // environment in which java code can run, hence it is a java web server
+
+
+    }
+
+    @GetMapping("/greet") 
+    public String greet(){
+        return "Hello there";
+    };
+
+}
