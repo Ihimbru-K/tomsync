91b84004c2bb6be58e716c98cbe1662f20653bf0
Ihimbru-k <kanihims25@gmail.com>
2024-05-09 12:44:33 +0100

    creating repository to create CRUD operations

---
+++ src/main/java/com/ihimbru/CustomerRepository.java
@@ -0,0 +1,11 @@
+package com.ihimbru;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+
+//This is an interface that would help us perform CRUD operations on the database without necessarily
+//writing sql commands directly
+public interface CustomerRepository extends JpaRepository<Customer, Integer > {
+
+
+}
