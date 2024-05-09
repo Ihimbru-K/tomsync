ad6baaa21b6fb287391f474ab65e2d432510f32c
Ihimbru-k <kanihims25@gmail.com>
2024-05-09 13:56:56 +0100

    completed customer repository, created method to fetch list of customers through customer repository, added customer data into database and tested API

--- src/main/java/com/ihimbru/Main.java
+++ src/main/java/com/ihimbru/Main.java
@@ -4,6 +4,7 @@ package com.ihimbru;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import java.util.List;
@@ -21,7 +22,15 @@ import java.util.List;
 // this annotation helps us create methods that will be exposed as rest APIs
 //all methods under this controller return a response in JSON format
 @RestController
+@RequestMapping("api/v1/customers")    // this will return an empty list from the getcustomers method
 public class Main {
+
+    private final CustomerRepository customerRepository;
+
+    public Main(CustomerRepository customerRepository) {
+        this.customerRepository = customerRepository;
+    }
+
     public static void main(String[] args) {
        // System.out.println("Hello World!");
         SpringApplication.run(Main.class, args); //runnable springboot application
@@ -33,11 +42,15 @@ public class Main {
        );
        return response;
     }
-
-
     record Person(String person){}
 
     record GreetResponse(String greet, Person person, List<String> favProgLang){}
+
+
+    @GetMapping
+    public List<Customer> getCustomers(){
+        return customerRepository.findAll();
+    }
 
 
 }
