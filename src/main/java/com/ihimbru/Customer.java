8764cb18d3b3d8667f962d2a8087ed2db5e5438c
Ihimbru-k <kanihims25@gmail.com>
2024-05-09 10:26:51 +0100

    mapped customer class to created database as entity using @Entity annotation and given it an id, Sequence Generator and generated value

--- src/main/java/com/ihimbru/Customer.java
+++ src/main/java/com/ihimbru/Customer.java
@@ -1,8 +1,22 @@
 package com.ihimbru;
 
+import jakarta.persistence.*;
+
 import java.util.Objects;
 
+
+@Entity //maps customer class to database as an entity
 public class Customer {
+
+    @Id
+    @SequenceGenerator(
+            name = "customer_id_sequence",
+            sequenceName = "customer_id_sequence"
+    )
+    @GeneratedValue(
+            strategy = GenerationType.SEQUENCE,
+            generator = "customer_id_sequence"
+    )
     private Integer id;
     private String name;
     private String email;
