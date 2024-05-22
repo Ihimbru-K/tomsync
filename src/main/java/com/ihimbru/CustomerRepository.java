1871b9d48acfa47ca5d3c6464eb712e320b2e453
Ihimbru-k <kanihims25@gmail.com>
2024-05-22 23:05:46 +0100

    done some modifications

--- src/main/java/com/ihimbru/CustomerRepository.java
+++ src/main/java/com/ihimbru/CustomerRepository.java
@@ -6,6 +6,11 @@ import org.springframework.data.jpa.repository.JpaRepository;
 //This is an interface that would help us perform CRUD operations on the database without necessarily
 //writing sql commands directly
 public interface CustomerRepository extends JpaRepository<Customer, Integer > {
+//
+//
+
+
+
 
 ///
 }
